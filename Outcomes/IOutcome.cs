925542338c8b2da29ce4c450698654699fe85e47
Brian <brianm@getkinetiq.com>
2015-06-16 22:39:27 -0400

    Fixed a problem where StatusCode was on IOutcome<T>, when it should have been on IOutcome.

--- Outcomes/IOutcome.cs
+++ Outcomes/IOutcome.cs
@@ -5,11 +5,11 @@ namespace Ether.Outcomes
     public interface IOutcome<TValue> : IOutcome
     {
         TValue Value { get; }
-        int? StatusCode { get; } 
     }
 
     public interface IOutcome
     {
+        int? StatusCode { get; } 
         bool Success { get; }
         List<string> Messages { get; }
         string ToMultiLine(string delimiter = null);
